1c1bc15a21faedfd806250537da20bbd2f1a2b10
forstie <forstie@us.ibm.com>
2016-11-29 10:53:34 -0600

    Update CURPTFGRP.sql
    Added "Description:" and used an SQL friendly version. (aka not so friendly to the awful STRSQL users)

--- sql_examples/CURPTFGRP.sql
+++ sql_examples/CURPTFGRP.sql
@@ -1,18 +1,15 @@
 --
+-- Description: PTF Group level checker
 -- Derive the IBM i operating system level and then 
--- determine the level of currency of PTF Groups.
--- Requires Admin server to be running
--- 
+-- determine the level of currency of PTF Groups
+--   
 With iLevel(iVersion, iRelease) AS
 (
 select OS_VERSION, OS_RELEASE from sysibmadm.env_sys_info
 )
-SELECT VARCHAR(GRP_CRNCY,25) AS "GRPCUR",
-GRP_ID, VARCHAR(GRP_TITLE, 20) AS "NAME",
-GRP_LVL, GRP_IBMLVL, GRP_LSTUPD,
-GRP_RLS, GRP_SYSSTS
-FROM iLevel, systools.group_ptf_currency P
-WHERE ptf_group_release =
-'R' CONCAT iVersion CONCAT iRelease concat '0'
-ORDER BY ptf_group_level_available -
-ptf_group_level_installed DESC
+  SELECT P.*
+     FROM iLevel, systools.group_ptf_currency P
+     WHERE ptf_group_release = 
+           'R' CONCAT iVersion CONCAT iRelease concat '0'
+     ORDER BY ptf_group_level_available -
+        ptf_group_level_installed DESC;
