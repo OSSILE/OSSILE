88dd6f5cac06e669366d3b05d6224fa5742e0607
chrish <chrish@192.168.110.1>
2016-12-09 19:35:55 -0500

    Test of subdirectory creation within c_examples

---
+++ c_examples/encrypt_sample/QCSRC/HELLOWORLD.C
@@ -0,0 +1,8 @@
+000100161209#include <stdio.h>
+000101161209#include <string.h>
+000102161209
+000103161209int main(int argc, char **argv) {
+000104161209
+000105161209printf("HelloWorld not encrypted obviously\n");
+000106161209exit(0);
+000107161209}
