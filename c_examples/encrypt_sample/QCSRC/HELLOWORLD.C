5a20c572cf6de42ae11302ddea1eb7f435434d52
chrish <chrish@192.168.110.1>
2016-12-10 12:29:56 -0500

    Adding back the test file with no sequence numbers

--- c_examples/encrypt_sample/QCSRC/HELLOWORLD.C
+++ c_examples/encrypt_sample/QCSRC/HELLOWORLD.C
@@ -1,8 +1,8 @@
-000100161209#include <stdio.h>
-000101161209#include <string.h>
-000102161209
-000103161209int main(int argc, char **argv) {
-000104161209
-000105161209printf("HelloWorld not encrypted obviously\n");
-000106161209exit(0);
-000107161209}
+#include <stdio.h>
+#include <string.h>
+ 
+int main(int argc, char **argv) {
+ 
+printf("HelloWorld\n");
+exit(0);
+}
