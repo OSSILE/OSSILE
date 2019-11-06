14281da7b635a1e0bade73870bbadfd3d057506d
Brian Garland <bjgarland@gmail.com>
2019-11-06 15:22:40 -0500

    update with last changes, point to correct parent repo

--- main/crtfrmstmf/crtfrmstmf.cmd
+++ main/crtfrmstmf/crtfrmstmf.cmd
@@ -5,16 +5,22 @@
 
              CMD        PROMPT('Create Object From Stream File')
 
-             PARM       KWD(OBJ) TYPE(QUAL1) RSTD(*NO) MIN(1) MAX(1) PROMPT('Object')
+             PARM       KWD(OBJ) TYPE(QUAL1) RSTD(*NO) MIN(1) MAX(1) +
+                          PROMPT('Object')
 
-             PARM       KWD(CMD) TYPE(*NAME) LEN(10) RSTD(*YES) VALUES(CRTCMD CRTBNDCL CRTCLMOD CRTDSPF CRTPRTF +
-                          CRTLF CRTPF CRTMNU CRTPNLGRP CRTQMQRY CRTSRVPGM CRTWSCST) MIN(1) CHOICE(*VALUES) PROMPT('Compile command')
+             PARM       KWD(CMD) TYPE(*NAME) LEN(10) RSTD(*YES) +
+                          VALUES(CRTCMD CRTBNDCL CRTCLMOD CRTDSPF CRTPRTF +
+                          CRTLF CRTPF CRTMNU CRTPNLGRP CRTQMQRY CRTSRVPGM +
+                          CRTWSCST) MIN(1) CHOICE(*VALUES) PROMPT('Compile +
+                          command')
 
-             PARM       KWD(SRCSTMF) TYPE(*PNAME) LEN(5000) MIN(1) VARY(*YES *INT2) CASE(*MIXED) PROMPT('Source +
+             PARM       KWD(SRCSTMF) TYPE(*PNAME) LEN(5000) MIN(1) +
+                          VARY(*YES *INT2) CASE(*MIXED) PROMPT('Source +
                           stream file')
 
-             PARM       KWD(PARMS) TYPE(*CHAR) LEN(2000) VARY(*YES *INT2) CASE(*MONO) PROMPT('Additional +
-                          parameters')
+             PARM       KWD(PARMS) TYPE(*CHAR) LEN(2000) VARY(*YES *INT2) +
+                          CASE(*MONO) PROMPT('Additional parameters')
 
  QUAL1:      QUAL       TYPE(*NAME) LEN(10) RSTD(*NO) MIN(1)
-             QUAL       TYPE(*NAME) LEN(10) RSTD(*NO) DFT(*CURLIB) SPCVAL((*CURLIB)) MIN(0) PROMPT('Library')
+             QUAL       TYPE(*NAME) LEN(10) RSTD(*NO) DFT(*CURLIB) +
+                          SPCVAL((*CURLIB)) MIN(0) PROMPT('Library')
